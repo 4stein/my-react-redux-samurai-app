cae9db65d925842b7905dfcd0204432ff94a304e
STEIN <doruchenko.m.i@gmail.com>
2021-01-14 15:42:47 +0200

    Updates

---
+++ server/server-api-json.js
@@ -0,0 +1,45 @@
+[
+  {
+  	id: 1, 
+  	followed: false, 
+  	name: 'Name 1', 
+  	status: 'boss',      
+  	location: {
+  		country: 'Ukraine',  
+  		city: 'Kiev'
+  	},   
+  	photo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Default_User_Logo.jpg'
+  },
+  {
+  	id: 2, 
+  	followed: true,  
+  	name: 'Name 2', 
+  	status: 'developer', 
+  	location: {
+  		country: 'Ukraine',  
+  		city: 'Lviv'
+  	},   
+  	photo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Default_User_Logo.jpg'
+  },
+  {
+  	id: 3, 
+  	followed: false, 
+  	name: 'Name 3', 
+  	status: 'manager',   
+  	location: {
+  		country: 'Belarus',  
+  		city: 'Minsk'
+  	},  photo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Default_User_Logo.jpg'
+  },
+  {
+  	id: 4, 
+  	followed: true,  
+  	name: 'Name 4', 
+  	status: 'designer',  
+  	location: {
+  		country: 'Russia',   
+  		city: 'Moscow'
+  	}, 
+  	photo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Default_User_Logo.jpg'
+  }
+]
